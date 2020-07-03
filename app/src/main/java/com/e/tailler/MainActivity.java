3f955e748bf2d4ad85b9e9d4069d64cef5adae99
merek7 <kmerek7@gmail.com>
2020-07-03 23:25:31 +0000

    creation de la page d'accueill

--- app/src/main/java/com/e/tailler/MainActivity.java
+++ app/src/main/java/com/e/tailler/MainActivity.java
@@ -2,13 +2,41 @@ package com.e.tailler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int HOME=5000;
+    //variable
+    Animation topAnim;
+    GifImageView gif;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_anim);
+
+        gif= findViewById(R.id.gifImageView);
+
+        gif.setAnimation(topAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, Login.class);;
+                startActivity(intent);
+                finish();
+            }
+        },HOME);
     }
 }
